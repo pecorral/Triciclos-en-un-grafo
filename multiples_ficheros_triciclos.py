7212fe6bec90f7e8a8cd46ce60b22b19e6a71493
pecorral <124246142+pecorral@users.noreply.github.com>
2023-04-28 13:12:56 +0200

    Add files via upload

---
+++ multiples_ficheros_triciclos.py
@@ -0,0 +1,51 @@
+from pyspark import SparkContext
+import sys
+
+
+def filtro1(linea):
+    dos_nodos=linea.split(',')
+    n1=dos_nodos[0]
+    n2=dos_nodos[1]
+    if n1<n2:
+        return (n1,n2)
+    elif n2>n1:
+        return (n2,n1)
+    else:
+        pass
+    
+def f1(nodo_lista):
+    nodo=nodo_lista[0]
+    lista=nodo_lista[1]
+    tricilos=[]
+    for i in range(len(lista)):
+        n=lista[i]
+        tricilos.append(((nodo,n),'exists'))
+        for j in range(i+1, len(lista)):
+            tricilos.append(((n,lista[j]),('pending', nodo)))
+    return tricilos
+
+
+def main(sc, filename_list):
+    grafo=sc.emptyRDD()
+    for filename in filename_list:
+        grafo = grafo.union(sc.textFile(filename))
+    gb=grafo.map(filtro1).distinct().filter(lambda x: x!=None)
+    adyacentes=gb.groupByKey().map(lambda x: (x[0],sorted(list(x[1])))).sortByKey()
+    triciclos=adyacentes.flatMap(f1).groupByKey()
+    lista_triciclos=[]
+    for pareja, mensajes in triciclos.collect():
+        mens=list(mensajes)
+        if len(mens)>1 and 'exists' in mens:
+            for m in mens:
+                if m!='exists': 
+                    lista_triciclos.append((m[1],pareja[0],pareja[1]))
+    print("Lista de triciclos", sorted(lista_triciclos))
+
+
+if __name__=="__main__":
+    if len(sys.argv) < 2:
+        print("Uso: python3 {0} <list of files>".format(sys.argv[0]))
+    else:
+        with SparkContext() as sc:
+            sc.setLogLevel("ERROR")
+            main(sc, sys.argv[1:])
